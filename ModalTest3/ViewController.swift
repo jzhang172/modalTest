889910bc9823f6ec5222ce0d37ce0477e999f72b
Jason Zhang <jason.zhang@ablevets.com>
2016-05-12 14:54:19 -0400

    Initial Commit

---
+++ ModalTest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalTest3
+//
+//  Created by AV-ZhangJ on 5/12/16.
+//  Copyright © 2016 AV-ZhangJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
